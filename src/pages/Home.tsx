e163f581a3d59e8e091aec7a2da001da51c47a1a
banasick <banasick@mail.kobe-c.ac.jp>
2026-05-06 18:12:42 +0900

    clean up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,7 @@
-// import reactLogo from "./../assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import { useState } from "react";
 import { Link } from "react-router";
-import LocaleSwitcher from "../components/LocaleSwitcher.tsx";
+// import LocaleSwitcher from "../components/LocaleSwitcher.tsx";
 import { useTranslation } from "react-i18next";
 import { Button } from "@/components/ui/button.tsx";
-// import { toast } from "sonner";
 
 export default function Home() {
   const { t } = useTranslation();
@@ -18,13 +14,15 @@ export default function Home() {
             KADE - Legacy Data Converter
           </h1>
 
-          <div className="ml-auto">
+          {/* <div className="ml-auto">
             <LocaleSwitcher />
-          </div>
+          </div> */}
         </header>{" "}
         <section className="mt-8 grid gap-6 md:grid-cols-2 items-center">
           <div className="text-center md:text-left gap-4 flex flex-col items-center md:items-start">
-            <p className="text-slate-700 mb-4 ">{t("Select Data Source")}</p>
+            <p className="text-slate-700 mb-4 text-2xl font-semibold">
+              {t("Select Data Source")}
+            </p>
 
             <div className="flex flex-col md:flex-row gap-4">
               <Button asChild variant="secondary">
@@ -43,25 +41,8 @@ export default function Home() {
                 </Link>
               </Button>
             </div>
-            {/* <Button
-              variant="outline"
-              onClick={() =>
-                toast("Event has been created", {
-                  description: "Sunday, December 03, 2023 at 9:00 AM",
-                  action: {
-                    label: "Undo",
-                    onClick: () => console.log("Undo"),
-                  },
-                })
-              }
-            >
-              Show Toast
-            </Button> */}
           </div>
         </section>
-        {/* <footer className="mt-8 text-center text-sm text-slate-500">
-          Made with ❤️ by Giacomo
-        </footer> */}
       </main>
     </div>
   );
